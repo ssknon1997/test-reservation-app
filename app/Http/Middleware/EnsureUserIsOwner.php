588011acaeed583476a33c63cc8e36b76077f3f8
non0921 <taruto7771@gmail.com>
2026-06-09 01:47:18 +0900

    feat: オーナー以外を弾くmiddlewareを作成,登録

---
+++ app/Http/Middleware/EnsureUserIsOwner.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureUserIsOwner
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (! $request->user()->isOwner()) {
+            abort(403, 'オーナーのみアクセスできます');
+        }
+        //次の処理へ進む
+        return $next($request);
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        //ownerというエイリアス(あだ名)を設定してweb.phpでmiddleware('owner')で呼び出せるようになる
+        $middleware->alias([
+            'owner' => \App\Http\Middleware\EnsureUserIsOwner::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
