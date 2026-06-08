facecf18a111470832f037ab2ea5373c5513de2c
non0921 <taruto7771@gmail.com>
2026-06-08 20:43:56 +0900

    feat: StoreShopRequestにバリデーション、エラーの翻訳を追加

---
+++ app/Http/Requests/StoreShopRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreShopRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        //userモデルのisownerメソッド
+        return $this->user()->isOwner();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+        'name' => ['required', 'string', 'max:255'],
+        'description' => ['nullable', 'string', 'max:1000'],
+        'addres' => ['required', 'string', 'max:255'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+        'name.required'   => '店舗名は必須です',
+        'name.max'        => '店舗名は255文字以内で入力してください',
+        'addres.required' => '住所は必須です',
+        'addres.max'      => '店舗名は必須です',
+        ];
+    }
+}
