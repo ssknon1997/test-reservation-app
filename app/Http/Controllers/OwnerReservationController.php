83481ee168f6f80d5a3daf890fceeaa34220627e
non0921 <taruto7771@gmail.com>
2026-06-10 22:12:18 +0900

    feat: OwnerReservationControllerにオーナーのみが可能な処理を追加

--- app/Http/Controllers/OwnerReservationController.php
+++ app/Http/Controllers/OwnerReservationController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Shop;
 
 class OwnerReservationController extends Controller
 {
-    //
+
+    public function index()
+    {
+        $reservations = auth()->user()
+            ->shops()
+            //1つのModelで複数のリレーションテーブルを取得する場合['', '']で書く
+            ->with(['reservations.user', 'reservations.shop'])
+            ->get()
+            ->pluck('reservations')
+            ->flatten()
+            ->sortByDesc('reserved_at');
+
+        return view('owner.reservations.index', compact('reservations'));
+    }
+
+    public function show(Shop $shop, $reservationId)
+    {
+        $reservation = $shop->reservations()->findOrFail($reservationId);
+        return view('owner.reservations.show', compact('reservation'));
+    }
 }
